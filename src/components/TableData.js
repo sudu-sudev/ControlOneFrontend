8af730b5e861397be40d18a8ec45b9d77d556680
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-10 16:05:51 +0530

    Update TableData.js

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const TableData = ({ data }) => {
     return (
-        <tr>
+        <tr className="hover:bg-gray-100 border-b dark:border-b-gray-300">
             <td className="text-left px-6 py-4 whitespace-nowrap">
                 <div className="text-sm text-gray-600 font-semibold">
                     {data.id}
@@ -16,7 +16,7 @@ const TableData = ({ data }) => {
             </td>
             <td className="text-left px-6 py-4 whitespace-nowrap">
                 <div className="text-sm text-gray-600 font-semibold">
-                    {data.steeringStatus}
+                    {data.downLidar}
                 </div>
             </td>
             <td className="text-left px-6 py-4 whitespace-nowrap">
@@ -25,7 +25,7 @@ const TableData = ({ data }) => {
                 </div>
             </td>
             {data.batteryStatus > 30 ? (
-                <td className="text-left px-6 py-4 whitespace-nowrap">
+                <td className="text-left px-6 py-4 whitespace-nowrap ">
                     <div className="text-sm text-green-600 font-semibold">
                         {data.batteryStatus}%
                     </div>
@@ -42,25 +42,25 @@ const TableData = ({ data }) => {
             </td> */}
             <td className="text-left px-6 py-4 whitespace-nowrap">
                 <div className="text-sm text-gray-600 font-semibold">
-                    {data.downLidar}
+                    {data.IPAddress}
                 </div>
             </td>
             {data.networkStrength === 'Strong' ? (
                 <td className="text-left px-6 py-4 whitespace-nowrap">
                     <div className="text-sm text-green-600 font-semibold">
-                        {data.networkStrength} 
+                        {data.networkStrength} ms
                     </div>
                 </td>
             ) : data.networkStrength === 'Medium' ?(
                 <td className="text-left px-6 py-4 whitespace-nowrap">
                     <div className="text-sm text-orange-400 font-semibold">
-                        {data.networkStrength} 
+                        {data.networkStrength} ms
                     </div>
                 </td>
             ) : (
                 <td className="text-left px-6 py-4 whitespace-nowrap">
                     <div className="text-sm text-red-600 font-semibold">
-                        {data.networkStrength} 
+                        {data.networkStrength} ms
                     </div>
                 </td>
             )}
