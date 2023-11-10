86de86dfb12cdf3d7e38cbbb9eef7ac997b512bf
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-10 16:06:53 +0530

    Update ForkLift.js

--- src/components/ForkLift.js
+++ src/components/ForkLift.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import forklift from '../fl2.png'
 import { BiSolidDownload } from 'react-icons/bi'
 import { VscDebugStart } from 'react-icons/vsc'
 import Box from './Box'
 import { VscVmActive } from 'react-icons/vsc'
+import forklift2 from '../pngwing.com.png'
 
 const ForkLift = () => {
     return (
-        <div className="container flex flex-col  min-w-full min-h-screen bg-slate-600">
+        <div className="container flex flex-col  min-w-full min-h-screen dark:bg-slate-600 bg-stone-100">
             <div>
                 <div className="flex justify-between p-4">
                     <div className="flex">
-                        <h1 className="text-orange-400 font-semibold text-3xl">
+                        <h1 className="text-orange-400 font-bold  text-3xl uppercase ">
                             Forklift Details
                         </h1>
                     </div>
@@ -24,9 +24,9 @@ const ForkLift = () => {
                     </div>
                 </div>
 
-                <div className="md:flex lg:flex xl:flex sm:flex 2xl:flex ">
+                <div className="md:flex lg:flex xl:flex sm:flex 2xl:flex p-4">
                     <div className="">
-                        <img src={forklift} alt="" className="h-60 w-80"></img>
+                        <img src={forklift2} alt="" className="h-56 w-80"></img>
                     </div>
                     <div className="p-4 pt-6 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
                         <Box
@@ -38,6 +38,7 @@ const ForkLift = () => {
                             height={36}
                             width={36}
                             xl={64}
+                            flex={'flex-col'}
                         />
                         <Box
                             title={'Experience'}
@@ -48,6 +49,7 @@ const ForkLift = () => {
                             height={36}
                             width={36}
                             xl={64}
+                            flex={'flex-col'}
                         />
                         <Box
                             title={'Experience'}
@@ -58,6 +60,7 @@ const ForkLift = () => {
                             height={36}
                             width={36}
                             xl={64}
+                            flex={'flex-col'}
                         />
                         <Box
                             title={'Experience'}
@@ -68,6 +71,7 @@ const ForkLift = () => {
                             height={36}
                             width={36}
                             xl={64}
+                            flex={'flex-col'}
                         />
                         <Box
                             title={'Experience'}
@@ -78,6 +82,7 @@ const ForkLift = () => {
                             height={36}
                             width={36}
                             xl={64}
+                            flex={'flex-col'}
                         />
                         <Box
                             title={'Experience'}
@@ -88,15 +93,18 @@ const ForkLift = () => {
                             height={36}
                             width={36}
                             xl={64}
+                            flex={'flex-col'}
                         />
                     </div>
                 </div>
             </div>
             <div className="flex justify-center mt-auto mb-auto">
-                <div className="flex bg-orange-400 justify-center items-center p-4 w-36 h-14 rounded-md hover:bg-orange-300">
-                    <button className="flex items-center">
-                        <VscDebugStart className="mr-4" />
-                        Start
+                <div className="flex bg-gradient-to-r bg-orange-400 justify-center items-center p-2 w-36 h-14  rounded-xl hover:bg-orange-300 hover:scale-105 transition ease-in-out delay-150">
+                    <button className="flex items-center ">
+                        <VscDebugStart className="mr-2 w-8 h-8" />
+                        <span className="text-2xl font-semibold animate-pulse">
+                            Start
+                        </span>
                     </button>
                 </div>
             </div>
