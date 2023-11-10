d643c6dc62f0ba1455e38e04f1b66e40afda1a8e
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-10 16:07:29 +0530

    Update Box.js

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const Box = ({ title, icon, completed, height, width, xl }) => {
+const Box = ({ title, icon, completed, height, width, xl, flex }) => {
     return (
         <div
-            className={`h-${height} w-${width}  xl:w-${xl}  bg-slate-800 shadow-xl rounded-xl hover:bg-slate-700 hover:scale-105 transition ease-in-out delay-150`}
+            className={`h-${height} w-${width}  xl:w-${xl}  dark:bg-gray-800 shadow-xl rounded-xl dark:hover:bg-slate-700 hover:scale-100 transition ease-in-out delay-0 bg-white hover:bg-gray-200 `}
         >
-            <div className="p-5 text-white">
-                <div className="">
-                    {icon}
-                    <h1 className="m-4">{title}</h1>
-                    <h2 className="m-4 ">{completed}</h2>
+            <div className="p-5 text-black dark:text-white font-semibold ">
+                <div className={`flex ${flex} justify-between`}>
+                    <div>{icon}</div>
+                    <div>
+                        <h1 className="m-4">{title}</h1>
+                        <h2 className="m-4 ">{completed}</h2>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
-
 export default Box
