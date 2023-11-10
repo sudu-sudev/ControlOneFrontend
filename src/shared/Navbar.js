79573e477414cfc6ec7e3242a51bbeaade663e39
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-10 16:17:54 +0530

    Update Navbar.js

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -51,6 +51,12 @@ const Navbar = () => {
                             <h1 className="uppercase">Users</h1>
                         </div>
                     </Link>
+                            <button
+                    className="w-8 h-8 flex justify-center items-center dark:bg-slate-500 bg-slate-200 dark:text-white  text-black text-xl rounded-full m-1"
+                    onClick={() => setTheme(!theme)}
+                >
+                    {theme === true ? <BsFillSunFill /> : <BsFillMoonFill />}
+                </button>
                 </div>
             </div>
             <div className="mb-3 flex  justify-center">
